907dca5d2da93f048e0f61961411b7d48e6074dd
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-03-19 10:50:41 -0600

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: solid 1px yellow;
+
+  h1 {
+    color: yellow;
+  }
+`;
 
 const App = () => {
+  const [swData, getSWData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((res) => {
+        getSWData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <StyledApp>
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        {swData.map((element) => {
+          return <Character key={element.name} data={element} />;
+        })}
+      </div>
+    </StyledApp>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,48 @@
 // Write your Character component here
+// Write your Character component here
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const keyframe = keyframes`
+100% {
+    opacity: 1;
+}
+`;
+
+const StyledCharacter = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2%;
+  color: yellow;
+  padding: 20px;
+  border: 1px solid yellow;
+  opacity: 0;
+  animation: ${keyframe} 3s ease-in-out forwards;
+
+  h2 {
+    font-size: 3rem;
+  }
+  span {
+    font-size: 2rem;
+  }
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default function Character({ data }) {
+  return (
+    <StyledCharacter>
+      <h2>{data.name}</h2>
+      <span>Stats:</span>
+      <ul>
+        <li>Birth Year: {data.birth_year}</li>
+        <li>Height: {data.height}cm</li>
+        <li>Mass: {data.mass}cm</li>
+        <li>Gender: {data.gender}</li>
+      </ul>
+    </StyledCharacter>
+  );
+}
